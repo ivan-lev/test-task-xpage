71088386dbde7e23a05516e954694dc1e42c14d0
Ivan Lev <fanatos@mail.ru>
2024-08-14 10:01:29 +0500

    feat: add Header component with adaptivity

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,42 @@
+import './Header.scss';
+
+import Logo from '../Logo/Logo';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <Logo />
+      <nav className="header__nav-links">
+        <ul className="header__nav-links-list">
+          <li className="header__nav-link-element">
+            <a href="#" className="header__nav-link">
+              Играть
+            </a>
+          </li>
+          <li className="header__nav-link-element">
+            <a href="#" className="header__nav-link">
+              Смотерть
+            </a>
+          </li>
+          <li className="header__nav-link-element">
+            <a href="#" className="header__nav-link">
+              Учиться
+            </a>
+          </li>
+          <li className="header__nav-link-element">
+            <a href="#" className="header__nav-link">
+              Мероприятия
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <button className="header__menu">
+        <span className="header__menu-text">Меню</span>
+      </button>
+      <div className="header__user-info">
+        <span className="header__user-name">геннадий П.</span>
+        <img className="header__user-photo" src="/images/user-photo.jpg" alt="Фото пользователя" />
+      </div>
+    </header>
+  );
+}
