477f3320b89dbfb63fdf32f9b652a38955f53eef
Ivan Lev <fanatos@mail.ru>
2024-08-14 16:12:42 +0500

    feat: add initial main section hero layout

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,5 +1,18 @@
 import './Main.scss';
 
 export default function Main() {
-  return <main className="content">Main here</main>;
+  return (
+    <main className="content">
+      <section className="border-rounded section content__hero">
+        <h2 className="content__hero-title"></h2>
+        <div className="content__hero-buttons">
+          <button className="border-rounded content__hero-button">Играй</button>
+          <button className="border-rounded content__hero-button">Учись</button>
+          <button className="border-rounded content__hero-button">Смотри</button>
+        </div>
+      </section>
+      <section className="content__play"></section>
+      <section className="content__learn"></section>
+    </main>
+  );
 }
