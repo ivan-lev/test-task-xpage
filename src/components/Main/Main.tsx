d012d16cc8c2813133180dae7b1336f5ceb9de1f
Ivan Lev <fanatos@mail.ru>
2024-08-14 16:45:44 +0500

    feat: add logic for changin hero titile

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,14 +1,46 @@
 import './Main.scss';
 
+import { useState } from 'react';
+
 export default function Main() {
+  const alternativeTitles = [
+    'Найди с кем поиграть в баскет',
+    'Играй в баскет',
+    'Учись играть в баскет',
+    'Смотри, как играют в баскет'
+  ];
+  const [heroTitle, setHeroTitle] = useState(alternativeTitles[0]);
+
+  const changeTitle = (titleNumber: number) => {
+    setHeroTitle(alternativeTitles[titleNumber]);
+  };
+
   return (
     <main className="content">
       <section className="border-rounded section content__hero">
-        <h2 className="content__hero-title"></h2>
+        <h2 className="content__hero-title">{heroTitle}</h2>
         <div className="content__hero-buttons">
-          <button className="border-rounded content__hero-button">Играй</button>
-          <button className="border-rounded content__hero-button">Учись</button>
-          <button className="border-rounded content__hero-button">Смотри</button>
+          <button
+            className="border-rounded content__hero-button"
+            onMouseEnter={() => changeTitle(1)}
+            onMouseLeave={() => changeTitle(0)}
+          >
+            Играй
+          </button>
+          <button
+            className="border-rounded content__hero-button"
+            onMouseEnter={() => changeTitle(2)}
+            onMouseLeave={() => changeTitle(0)}
+          >
+            Учись
+          </button>
+          <button
+            className="border-rounded content__hero-button"
+            onMouseEnter={() => changeTitle(3)}
+            onMouseLeave={() => changeTitle(0)}
+          >
+            Смотри
+          </button>
         </div>
       </section>
       <section className="content__play"></section>
