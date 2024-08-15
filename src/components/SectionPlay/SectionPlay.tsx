2eb7433daf876fd1c25d9903b03a42677de360a7
Ivan Lev <fanatos@mail.ru>
2024-08-15 22:14:25 +0500

    feat: implement Card components

--- src/components/SectionPlay/SectionPlay.tsx
+++ src/components/SectionPlay/SectionPlay.tsx
@@ -1,5 +1,9 @@
 import './SectionPlay.scss';
 
+import Card from '../Card/Card';
+
+import { cardsArray } from '../../variables/cardsData';
+
 export default function SectionPlay(): JSX.Element {
   return (
     <section className="section section_titled play">
@@ -11,59 +15,9 @@ export default function SectionPlay(): JSX.Element {
       </div>
 
       <div className="play__cards">
-        <div className="border-rounded play__card">
-          <img
-            className="play__card-img"
-            src="/public/images/card-image-1.jpg"
-            alt="Превью карточки"
-          />
-          <div className="play__card-text">
-            <h3 className="title3">Найди себе команду</h3>
-            <span className="play__card-description">
-              Игры на районе - это возможность найти с кем поиграть в баскетбол в выходные, или
-              создать свою игру и пригласить других игроков присоединиться.
-            </span>
-          </div>
-          <a href="#" className="border-rounded button button_active play__card-button">
-            Текст кнопки
-          </a>
-        </div>
-
-        <div className="border-rounded play__card">
-          <img
-            className="play__card-img play__card-img_unbordered"
-            src="/public/images/card-image-2.jpg"
-            alt="Превью карточки"
-          />
-          <div className="play__card-text">
-            <h3 className="title3">Создавай команды</h3>
-            <span className="play__card-description">
-              Игры на районе - это возможность найти с кем поиграть в баскетбол в выходные, или
-              создать свою игру и пригласить других игроков присоединиться.
-            </span>
-          </div>
-          <a href="#" className="border-rounded button button_active play__card-button">
-            Текст кнопки
-          </a>
-        </div>
-
-        <div className="border-rounded play__card">
-          <img
-            className="play__card-img"
-            src="/public/images/card-image-3.jpg"
-            alt="Превью карточки"
-          />
-          <div className="play__card-text">
-            <h3 className="title3">Участвуй в турнирах</h3>
-            <span className="play__card-description">
-              Участвуйте в турнирах вместе с вашей командой. Будьте активными, побеждайте и
-              получайте ачивки и призы от наших спонсоров.
-            </span>
-          </div>
-          <a href="#" className="border-rounded button button_active  play__card-button">
-            Текст кнопки
-          </a>
-        </div>
+        {cardsArray.map(card => (
+          <Card card={card} />
+        ))}
       </div>
     </section>
   );
---
+++ src/variables/cardsData.ts
@@ -0,0 +1,25 @@
+import { CardsArray } from '../types/cardType';
+
+export const cardsArray: CardsArray = [
+  {
+    title: 'Найди себе команду',
+    text: 'Игры на районе - это возможность найти с кем поиграть в баскетбол в выходные, или создать свою игру и пригласить других игроков присоединиться.',
+    buttonText: 'Текст кнопки',
+    image: '/public/images/card-image-1.jpg',
+    isImageRounded: true
+  },
+  {
+    title: 'Создавай команды',
+    text: 'Игры на районе - это возможность найти с кем поиграть в баскетбол в выходные, или создать свою игру и пригласить других игроков присоединиться.',
+    buttonText: 'Текст кнопки',
+    image: '/public/images/card-image-2.jpg',
+    isImageRounded: false
+  },
+  {
+    title: 'Участвуй в турнирах',
+    text: 'Участвуйте в турнирах вместе с вашей командой. Будьте активными, побеждайте и получайте ачивки и призы от наших спонсоров.',
+    buttonText: 'Текст кнопки',
+    image: '/public/images/card-image-3.jpg',
+    isImageRounded: true
+  }
+];
