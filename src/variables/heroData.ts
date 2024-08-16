d64454f2192b03750c578dc366dda145574fc73f
Ivan Lev <fanatos@mail.ru>
2024-08-16 15:19:10 +0500

    fix: update images paths

--- src/variables/heroData.ts
+++ src/variables/heroData.ts
@@ -5,7 +5,7 @@ export const heroData: HeroDataArray = [
     title: 'Найди с кем поиграть в баскет',
     backgroundImage: '/images/hero-background.webp'
   },
-  { title: 'Играй в баскет', backgroundImage: '/images/hero-background-1.webp' },
-  { title: 'Учись играть в баскет', backgroundImage: '/images/hero-background-2.webp' },
-  { title: 'Смотри, как играют в баскет', backgroundImage: '/images/hero-background-3.webp' }
+  { title: 'Играй в баскет', backgroundImage: 'images/hero-background-1.webp' },
+  { title: 'Учись играть в баскет', backgroundImage: 'images/hero-background-2.webp' },
+  { title: 'Смотри, как играют в баскет', backgroundImage: 'images/hero-background-3.webp' }
 ];
