5fdca2e49503b77c35c3ee66d80c78450b91150f
Ivan Lev <fanatos@mail.ru>
2024-08-16 14:53:20 +0500

    refactor: delete 'public' from paths

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -3,7 +3,7 @@ import './Logo.scss';
 export default function Logo(): JSX.Element {
   return (
     <a href="#">
-      <img src="../../../public/logo.svg" alt="Логотип" className="hover-600 logo" />
+      <img src="/logo.svg" alt="Логотип" className="hover-600 logo" />
     </a>
   );
 }
